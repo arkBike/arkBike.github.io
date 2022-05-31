d8ae5633d72fe929585b61e5e0c38a40b5a020af
arkBike <ejun92@gmail.com>
2022-05-31 10:25:56 +0900

    업데이트 반영

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-let header = $('header'),
-		menu = header.find('nav > ul > li'),
-		headerHeight = header.outerHeight(),
-		newHeight = 0,
-		subMenu = menu.find('ul');
+// let header = $('.container'),
+// 		menu = header.find('nav > ul > li'),
+// 		headerHeight = header.outerHeight(),
+// 		newHeight = 0,
+// 		subMenu = menu.find('.dropdown');
 
-subMenu.each(function(){
-	if($(this).outerHeight() > newHeight){
-		newHeight = headerHeight +$(this).outerHeight();
-	}
-});
+// subMenu.each(function(){
+// 	if($(this).outerHeight() > newHeight){
+// 		newHeight = headerHeight +$(this).outerHeight();
+// 	}
+// });
 
-menu.hover(
-	function(){
-		header.stop().animate({height: newHeight});
-	},
-	function(){
-		header.stop().animate({height: headerHeight});
-	}
-);
+// menu.hover(
+// 	function(){
+// 		header.stop().animate({height: newHeight});
+// 	},
+// 	function(){
+// 		header.stop().animate({height: headerHeight});
+// 	}
+// );
