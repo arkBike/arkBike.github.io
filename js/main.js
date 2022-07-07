3a8a6e33c0f35c54053d17869412b3eab05e1261
arkBike <ejun92@gmail.com>
2022-07-07 21:57:00 +0900

    js 수정

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-// let header = $('.container'),
-// 		menu = header.find('nav > ul > li'),
-// 		headerHeight = header.outerHeight(),
-// 		newHeight = 0,
-// 		subMenu = menu.find('.dropdown');
-
-// subMenu.each(function(){
-// 	if($(this).outerHeight() > newHeight){
-// 		newHeight = headerHeight +$(this).outerHeight();
-// 	}
-// });
-
-// menu.hover(
-// 	function(){
-// 		header.stop().animate({height: newHeight});
-// 	},
-// 	function(){
-// 		header.stop().animate({height: headerHeight});
-// 	}
-// );
-
-
 $('.mainslides').bxSlider({
     controls: false,
     pager: false
