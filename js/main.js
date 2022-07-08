3d6b174e01feae3ff8610d47697f17f488b00f16
arkbike <ejun92@gmail.com>
2022-07-08 14:41:07 +0900

    드롭다운 메뉴를 위한 스크립트 작성

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-$('.mainslides').bxSlider({
-    controls: false,
-    pager: false
+let header = $('header'),
+	menu = header.find('nav > ul > li'),
+	headerHeight = header.outerHeight(),
+	newHeight = 0,
+	subMenu = menu.find('ul');
+
+subMenu.each(function(){
+	if($(this).outerHeight() > newHeight){
+		newHeight = headerHeight +$(this).outerHeight();
+	}
 });
+
+menu.hover(
+	function(){
+		header.stop().animate({height: newHeight});
+	},
+	function(){
+		header.stop().animate({height: headerHeight});
+	}
+);
+
+
+// $('.mainslides').bxSlider({
+//     controls: false,
+//     pager: false
+// });
