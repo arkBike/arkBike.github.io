7521d546c43892daebc29e722d129cd1839ecec2
arkbike <ejun92@gmail.com>
2022-07-08 18:14:17 +0900

    검색 모달 스크립트 추가, 슬릭 js 스크립트 추가중

--- js/main.js
+++ js/main.js
@@ -19,6 +19,46 @@ menu.hover(
 	}
 );
 
+$('.fa-magnifying-glass').click(function(){
+	$('.main_search_modal').fadeIn().addClass('active'); 
+	$('body').css({overflow:'hidden'});
+	$('.main_search_modal input').focus();
+});
+$('.fa-x').click(function(){
+	$('.main_search_modal').fadeOut().removeClass('active'); 
+	$('body').css({overflow:'auto'});
+});
+
+let wind = $(window),
+	scrollHeader = $('header'),
+	headerOffsetTop = scrollHeader.offset().top;
+
+	window.scroll(function(){
+		if($(this).scrollTop() >= headerOffsetTop){
+			scrollHeader.addClass('sticky');
+		}
+		else{
+			scrollHeader.removeClass('sticky');
+		}
+	});
+
+
+	// $('.mainslides').slick({
+        
+    // });
+	$('.mainslides').slick();
+	// $window.scroll(function(){
+	// 	if($(this).scrolltop() > 100){
+	// 		if(!$header.hasClass('samll')){
+	// 			$header.addClass('samll');
+	// 		}
+	// 	}
+	// 	else{
+	// 		if($header.hasClass('samll')){
+	// 			$header.removeClass('samll');
+	// 		}
+	// 	}
+	// });
 
 // $('.mainslides').bxSlider({
 //     controls: false,
