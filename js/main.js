08eff03499b8b0ba8fc272d49045478cc24ae4c8
arkBike <ejun92@gmail.com>
2022-07-09 17:18:25 +0900

    Back to Top 버튼 스크립트 작성

--- js/main.js
+++ js/main.js
@@ -101,3 +101,18 @@ mainsSlides.addEventListener('mouseover', ()=>{
 mainsSlides.addEventListener('mouseout', ()=>{
     startAutoSlide();
 });
+
+// Back to Top 버튼
+let btn = $('.btt');
+$(window).scroll(function(){
+	if($(window).scrollTop() > 300){
+		btn.addClass('show');
+	}
+	else{
+		btn.removeClass('show');
+	}
+});
+btn.on('click',function(e){
+	e.preventDefault();
+	$('html, body').animate({scrollTop: 0}, '300');
+});
